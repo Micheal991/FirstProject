2564bcc36bcb99b3bba07beaf285a64978da59c0
usifoh <usifohanyi@gmail.com>
2022-08-27 16:58:01 +0100

    hello

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
+
 import requests
-try:
-    result = requests.get('https://google.com')
-    print(result)
-except :
-    print('unsuccessful')
+def caller(url):
+    
+    with open('domains.txt','r') as  subdomains:
+        for subdomain in subdomains:
+           subdomain_line = subdomain.strip()
+           try:
+                response = requests.get('https://'+subdomain_line+'.'+url)   
+                if response.status_code == 200:
+                    print('[+] successful https://'+subdomain_line+'.'+url+' [+]')   
+           except :
+             pass
+
+
+
+
+caller("google.com")
